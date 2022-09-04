e2d4e1f3805f38fd1e991fcab3e3ca0ac6d15b55
JoseDanielF <danielduartefilho.df@gmail.com>
2022-09-03 22:51:29 -0300

    Medico Test

--- test/models/medico_test.rb
+++ test/models/medico_test.rb
@@ -1,9 +1,23 @@
 require "test_helper"
 
 class MedicoTest < ActiveSupport::TestCase
-  test 'criando medico corretamente' do
-    medico = Medico.new nome: 'Rodrigo Andrade', cpf: '706.508.980-01', email: 'rodrigo@rodrigo.com',
-                        especialidade: 'Reumatologista', crm: '123456'
+  test "criar medico valido" do
+    medico = Medico.new(nome:"Arthur", cpf:"013.862.694.41", email:"arthurdhiego@gmail.com", especialidade:"Cardiologista", crm:"19871234")
     assert medico.save
+  end
+
+  test "criar medico invalido" do
+    medico = Medico.new(nome:"", cpf:"013.862.694.41", email:"arthurdhiego@gmail.com", especialidade:"Cardiologista", crm:"19871234")
+    assert_not medico.save
+  end
+
+  test "criar medico sem email" do
+    medico = Medico.new(nome:"Arthur", cpf:"013.862.694.41", email:"", especialidade:"Cardiologista", crm:"19871234")
+    assert_not medico.save
+  end
+
+  test "criar medico sem crm" do
+    medico = Medico.new(nome:"Arthur", cpf:"013.862.694.41", email:"arthurdhiego@gmail.com", especialidade:"Cardiologista", crm:"")
+    assert_not medico.save
   end
 end
